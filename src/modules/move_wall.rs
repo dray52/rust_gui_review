4c3d59fdcc3db769ae5d9d23a46eb6ad79df36ea
dray52 <levesquedraydon@gmail.com>
2026-03-12 09:41:43 -0400

    web wall

--- src/modules/move_wall.rs
+++ src/modules/move_wall.rs
@@ -4,65 +4,56 @@ pub mod move_wall;
 
 use crate::modules::move_wall::MoveWall;
 */
-use macroquad::prelude::*;
-use crate::modules::still_image::StillImage;
 use crate::modules::scale::use_virtual_resolution;
+use crate::modules::still_image::StillImage;
+use macroquad::prelude::*;
 pub struct MoveWall {
     view: StillImage,
     move_speed: f32,
     movement: Vec2,
     direction: String,
-    
-
-
 }
 
-
-
-impl MoveWall{
- pub async fn new(
-        asset_path: &str, 
-        width: f32, 
-        height: f32, 
-        x: f32, 
+impl MoveWall {
+    pub async fn new(
+        asset_path: &str,
+        width: f32,
+        height: f32,
+        x: f32,
         y: f32,
         stretch_enabled: bool,
         zoom_level: f32,
-        direction: String) -> MoveWall{
-
-      MoveWall {
+        direction: String,
+    ) -> MoveWall {
+        MoveWall {
             view: StillImage::new(asset_path, width, height, x, y, stretch_enabled, zoom_level).await,
             move_speed: 200.0, // Default speed
             movement: Vec2::ZERO,
             direction,
-            
         }
-        }
+    }
 
-pub fn draw(&self) {
+    pub fn draw(&self) {
         // Only draw if the label is visible
-       self.view.draw();
-        }
-    
-#[allow(unused)]
- //change speed
+        self.view.draw();
+    }
+
+    #[allow(unused)]
+    //change speed
     pub fn set_speed(&mut self, move_speed: f32) -> &mut Self {
         self.move_speed = move_speed;
         self
     }
-
-   
-    
 
     // Getter for position as Vec2
     #[allow(unused)]
     pub fn get_position(&self) -> Vec2 {
         Vec2::new(self.view.get_x(), self.view.get_y())
     }
-    
+
     // Getter for visibility
     #[allow(unused)]
-  
+
     pub fn view_player(&self) -> &StillImage {
         &self.view
     }
@@ -73,7 +64,7 @@ pub fn draw(&self) {
         self.view.set_y(y);
         self
     }
-     pub fn get_x(&self) -> f32 {
+    pub fn get_x(&self) -> f32 {
         self.view.get_x()
     }
 
@@ -92,36 +83,31 @@ pub fn draw(&self) {
     pub fn set_y(&mut self, y: f32) {
         self.view.set_y(y);
     }
-     #[allow(unused)]
-     pub fn pos(&self) -> Vec2 {
+    #[allow(unused)]
+    pub fn pos(&self) -> Vec2 {
         vec2(self.view.get_x(), self.view.get_y())
     }
     #[allow(unused)]
     pub fn get_speed(&self) -> f32 {
         self.move_speed
     }
-   
- #[allow(unused)]
-    pub fn moveing(&mut self, width: f32, hight: f32, virtual_width: f32, virtual_height: f32)  {
-        
+
+    #[allow(unused)]
+    pub fn moveing(&mut self, width: f32, hight: f32, virtual_width: f32, virtual_height: f32) {
         if self.direction == "horizontal" {
             self.movement.x = self.move_speed * get_frame_time();
             self.movement.y = 0.0;
-            self.set_x(self.get_x()+self.movement.x);
-            if self.get_x() > virtual_width-width|| self.get_x() < 0.0{
+            self.set_x(self.get_x() + self.movement.x);
+            if self.get_x() > virtual_width - width || self.get_x() < 0.0 {
                 self.move_speed = self.move_speed * -1.0;
-                
             }
-        } else  if self.direction == "vertical" {
+        } else if self.direction == "vertical" {
             self.movement.y = self.move_speed * get_frame_time();
             self.movement.x = 0.0;
-             self.set_y(self.get_y()+self.movement.y);
-            if self.get_y() > virtual_height-hight|| self.get_y() < 0.0{
+            self.set_y(self.get_y() + self.movement.y);
+            if self.get_y() > virtual_height - hight || self.get_y() < 0.0 {
                 self.move_speed = self.move_speed * -1.0;
-                
             }
         }
-       
     }
-
-    }
+}
--- src/screen2.rs
+++ src/screen2.rs
@@ -1,150 +1,136 @@
-
-
-use macroquad::prelude::*;
-use crate::modules::still_image::StillImage;
-use crate::modules::collision::{check_collision};
-use crate::modules::text_button::TextButton;
+use crate::modules::collision::check_collision;
 use crate::modules::label::Label;
-use crate::modules::player::Player;
 use crate::modules::move_wall::MoveWall;
-use crate::modules::scale::use_virtual_resolution;
+use crate::modules::player::Player;
+use crate::modules::scale::{VIRTUAL_RESOLUTION, use_virtual_resolution};
+use crate::modules::still_image::StillImage;
+use crate::modules::text_button::TextButton;
+use macroquad::prelude::*;
 pub async fn run() -> String {
-    const VIRTUAL_WIDTH: f32 = 1024.0;
-    const VIRTUAL_HEIGHT: f32 = 768.0;
-    
+   // const VIRTUAL_WIDTH: f32 = 1024.0;
+   // const VIRTUAL_HEIGHT: f32 = 768.0;
+
     let mut amongus = Player::new(
         "assets/amongus.png",
-        50.0,  // width
-        50.0,  // height
-        70.0,  // x position
-        0.0,   // y position
-        true,   // Enable stretching
-        1.0,    // Normal zoom (100%)
-    ).await;
+        50.0, // width
+        50.0, // height
+        70.0, // x position
+        0.0,  // y position
+        true, // Enable stretching
+        1.0,  // Normal zoom (100%)
+    )
+    .await;
 
-     let mut wall = MoveWall::new(
+    let mut wall = MoveWall::new(
         "assets/wall.png",
-        100.0,  // width
+        100.0, // width
         50.0,  // height
-        200.0,  // x position
-        400.0,   // y position
-        true,   // Enable stretching
-        1.0,    // Normal zoom (100%)
-        "horizontal".to_string()
-    ).await;
+        200.0, // x position
+        400.0, // y position
+        true,  // Enable stretching
+        1.0,   // Normal zoom (100%)
+        "horizontal".to_string(),
+    )
+    .await;
 
     let maze = StillImage::new(
         "assets/maze.png",
-        1024.0,  // width
+        1024.0, // width
         768.0,  // height
-        0.0,  // x position
-        0.0,   // y position
+        0.0,    // x position
+        0.0,    // y position
         true,   // Enable stretching
         1.0,    // Normal zoom (100%)
-    ).await;
-     let mut key = StillImage::new(
+    )
+    .await;
+    let mut key = StillImage::new(
         "assets/key.png",
         40.0,  // width
         40.0,  // height
-        800.0,  // x position
-        30.0,   // y position
-        true,   // Enable stretching
-        1.0,    // Normal zoom (100%)
-    ).await;
-     let mut door = StillImage::new(
+        800.0, // x position
+        30.0,  // y position
+        true,  // Enable stretching
+        1.0,   // Normal zoom (100%)
+    )
+    .await;
+    let mut door = StillImage::new(
         "assets/door.png",
-        200.0,  // width
-        500.0,  // height
-        -30.0,  // x position
-        513.0,   // y position
-        true,   // Enable stretching
-        1.0,    // Normal zoom (100%)
-    ).await;
+        200.0, // width
+        500.0, // height
+        -30.0, // x position
+        513.0, // y position
+        true,  // Enable stretching
+        1.0,   // Normal zoom (100%)
+    )
+    .await;
     // Speed of movement in pixels per second
 
-
- let btn_exit = TextButton::new(
-        950.0,
-        20.0,
-        50.0,
-        50.0,
-        "X",
-        BLUE,
-        GREEN,
-        30
-    );
-     let mut lbl_time = Label::new("Time", 30.0, 20.0, 30);
-      let mut lbl_timer = Label::new("timer", 100.0, 20.0, 30);
-      lbl_time.with_colors(WHITE, None);
-      lbl_timer.with_colors(WHITE, None);
-      lbl_time.with_border(BLACK, 0.0);
-      lbl_timer.with_border(BLACK, 0.0);
-      let start_time = get_time();
+    let btn_exit = TextButton::new(950.0, 20.0, 50.0, 50.0, "X", BLUE, GREEN, 30);
+    let mut lbl_time = Label::new("Time", 30.0, 20.0, 30);
+    let mut lbl_timer = Label::new("timer", 100.0, 20.0, 30);
+    lbl_time.with_colors(WHITE, None);
+    lbl_timer.with_colors(WHITE, None);
+    lbl_time.with_border(BLACK, 0.0);
+    lbl_timer.with_border(BLACK, 0.0);
+    let start_time = get_time();
 
     loop {
-        use_virtual_resolution(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
-        #[allow(unused)]
-        let mut timer = get_time() - start_time;
+        use_virtual_resolution(1024.0, 768.0);
+       
+        let timer = get_time() - start_time;
 
-
-if timer != start_time{
- lbl_timer.set_text(format!("{:.0}", timer));
-}
-if timer >= 60.0 {
+        if timer != start_time {
+            lbl_timer.set_text(format!("{:.0}", timer));
+        }
+        if timer >= 60.0 {
             return "screen1".to_string();
         }
-
 
         clear_background(DARKGRAY);
-        
 
-    // Direction to move in
-    
-if timer>0.1{
-    
-   
-    // Save old position in case of collision
-   amongus.moveing();
-let old_pos = amongus.pos();
-if amongus.move_check_collision_x(&maze){
-    amongus.set_x(old_pos.x);
-}
+        // Direction to move in
 
-if amongus.move_check_collision_y(&maze){
-    amongus.set_y(old_pos.y);
-}
-if check_collision(amongus.view_player(), wall.view_player(), 1){
-amongus.set_y(wall.get_y()-50.0);
-if check_collision(amongus.view_player(), &maze, 1){
-    amongus.set_y(amongus.get_y() + 10.0);
-}
-}
-if amongus.get_x() < 40.0 && amongus.get_y() < 580.0 && amongus.get_y() > 537.0 {
-            amongus.set_y(amongus.get_y() + 20.0);
-            
+        if timer > 0.1 {
+            // Save old position in case of collision
+            amongus.moveing();
+            let old_pos = amongus.pos();
+            if amongus.move_check_collision_x(&maze) {
+                amongus.set_x(old_pos.x);
+            }
+
+            if amongus.move_check_collision_y(&maze) {
+                amongus.set_y(old_pos.y);
+            }
+            if check_collision(amongus.view_player(), wall.view_player(), 1) {
+                amongus.set_y(wall.get_y() - 50.0);
+                if check_collision(amongus.view_player(), &maze, 1) {
+                    amongus.set_y(amongus.get_y() + 10.0);
+                }
+            }
+            if amongus.get_x() < 40.0 && amongus.get_y() < 580.0 && amongus.get_y() > 537.0 {
+                amongus.set_y(amongus.get_y() + 20.0);
+            }
+
+            if btn_exit.click() {
+                return "screen1".to_string();
+            }
+            wall.moveing(100.0, 50.0, VIRTUAL_RESOLUTION.take().0, VIRTUAL_RESOLUTION.take().1);
+            if check_collision(wall.view_player(), amongus.view_player(), 1) {
+                amongus.set_y(old_pos.y);
+                amongus.set_x(old_pos.x);
+            }
+            if check_collision(amongus.view_player(), &key, 1) {
+                key.clear();
+                door.clear();
+            }
+            if check_collision(amongus.view_player(), &door, 1) {
+                amongus.set_y(old_pos.y);
+            }
+
+            if amongus.get_x() < 70.0 && amongus.get_y() > 880.0 {
+                return "screen1".to_string();
+            }
         }
-
-     if btn_exit.click() {
-            return "screen1".to_string();
-        }
-        wall.moveing(100.0, 50.0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
-if check_collision(wall.view_player(), amongus.view_player(), 1){
-    amongus.set_y(old_pos.y);
-    amongus.set_x(old_pos.x);
-}
-if check_collision(amongus.view_player(), &key, 1){
-    key.clear();
-    door.clear();
-
-}
-if check_collision(amongus.view_player(), &door, 1){
-    amongus.set_y(old_pos.y);
-}
-
-
-        if amongus.get_x() <70.0 && amongus.get_y() > 880.0 {
-            return "screen1".to_string();
-        }}
         maze.draw();
         amongus.draw();
         lbl_time.draw();
